4d93952c4aff27ae928b4f97d2a062c60c5b1da4
Matheus Antonio <dev.matheus.antonio@outlook.com>
2024-12-15 22:40:29 -0300

    6 - 1
    creating a function that adds two arrays

---
+++ js/6-luckyNumbers.js
@@ -0,0 +1,11 @@
+console.log("Lucky Numbers exercises");
+
+console.log("----------------------");
+
+function twoSum(array1, array2) {
+  return Number(array1.join('')) + Number(array2.join(''))
+}
+
+console.log(twoSum([1, 2, 3], [0, 7]));
+
+console.log("----------------------");
