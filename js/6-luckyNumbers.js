cd69925a1af903810b8c5f202423c414cd44772a
Matheus Antonio <dev.matheus.antonio@outlook.com>
2024-12-15 23:11:20 -0300

    6 - 2
    creating a function that identifies whether the number is a palindrome or not, return true or false.

--- js/6-luckyNumbers.js
+++ js/6-luckyNumbers.js
@@ -3,9 +3,25 @@ console.log("Lucky Numbers exercises");
 console.log("----------------------");
 
 function twoSum(array1, array2) {
-  return Number(array1.join('')) + Number(array2.join(''))
+  return Number(array1.join("")) + Number(array2.join(""));
 }
 
 console.log(twoSum([1, 2, 3], [0, 7]));
 
 console.log("----------------------");
+
+function luckyNumber(value) {
+  let slices = value.toString().split("").map(Number);
+  let reverse = slices.reverse().join("");
+
+  if (Number(reverse) === value) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(luckyNumber(1));
+
+console.log("----------------------");
+
